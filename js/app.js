794a5ad5e8f78ee4dcfb089834a5c97737592e4e
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-05-13 11:46:46 -0700

    Comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
 // Settings and options
 let options = new Map();
-options.set("includeFileDialog", false);
-options.set("uploadOnDrop", false);
-options.set("useExistingForm", false);
-options.set("dropZoneSelector", "body");
-options.set("formId", "existing-form");
-options.set("formAction", "/test/drag/drop/upload");
-options.set("formContainerId", "form-container");
+options.set("includeFileDialog", false);            // Show the file input element?
+options.set("uploadOnDrop", false);                 // Upload the file when it is dropped?
+options.set("useExistingForm", false);              // Use a form that already exists in your page?
+options.set("dropZoneSelector", "body");            // The id of the element that you want to be the drop zone?
+options.set("formId", "existing-form");             // The id of the form.  Either the Id of a pre-existing form, or the id that will be given to the form rendered by the js. 
+options.set("formAction", "/test/drag/drop/upload");// The action attribute that will be added to the form rendered by the js.  
+options.set("formContainerId", "form-container");   // The Id that you want set to the form container element.
 
 let dragDrop = new DragDropFileUpload(options);
 
