ee2e91931a7e4f2907824d72357f16e3b44f6744
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-05-13 11:53:41 -0700

    Setting change.

--- examples/app.js
+++ examples/app.js
@@ -3,7 +3,7 @@
 let options = new Map();
 options.set("includeFileDialog", false);            // Show the file input element?
 options.set("uploadOnDrop", false);                 // Upload the file when it is dropped?
-options.set("useExistingForm", false);              // Use a form that already exists in your page?
+options.set("useExistingForm", true);              // Use a form that already exists in your page?
 options.set("dropZoneSelector", "body");            // The id of the element that you want to be the drop zone?
 options.set("formId", "existing-form");             // The id of the form.  Either the Id of a pre-existing form, or the id that will be given to the form rendered by the js. 
 options.set("formAction", "/test/drag/drop/upload");// The action attribute that will be added to the form rendered by the js.  
